d368af7fbb2716b0f6a56fcd1949bae127221ddd
Sarveshjn <74890901+Sarveshjn@users.noreply.github.com>
2020-11-23 21:15:43 +0530

    Add files via upload

---
+++ DAY-1 TASK.py
@@ -0,0 +1,28 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Thirty days thirty hours cv")
+Thirty days thirty hours cv
+>>> hours = "thirty"
+>>> print (hours)
+thirty
+>>> days = "thirty days"
+>>> print(days[2])
+i
+>>> lineleng ="I love python programming"
+>>> print(lineleng[6:26])
+ python programming
+>>> print(lineleng.lower())
+i love python programming
+>>> print(lineleng.upper())
+I LOVE PYTHON PROGRAMMING
+>>> a="30 Days"
+>>> b="30 hours"
+>>> print(a+" "+b)
+30 Days 30 hours
+>>> 
+>>> print ("day one task")
+day one task
+>>> str ="I love python programming"
+>>> print(str[3:])
+ove python programming
+>>> 
