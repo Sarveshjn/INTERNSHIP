97e2fe48620a288c6e392e62c367ee59657e2162
Sarveshjn <74890901+Sarveshjn@users.noreply.github.com>
2020-11-24 21:02:20 +0530

    Add files via upload

---
+++ DAY-2 TASK.py
@@ -0,0 +1,32 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #DAY-2 TASK
+>>> 
+>>> #EXERCISE=1
+>>> 
+>>> a=b=c=40
+>>> 
+>>> a=a//10
+>>> b=b*50
+>>> c=c+60
+>>> print(a,b,c)
+4 2000 100
+>>> 
+>>> #EXERCISE=2
+>>> 
+>>> word="JSJPN"
+>>> word=word[:2]+'G'+word[3:]
+>>> print(word)
+JSGPN
+>>> 
+>>> #EXERCISE=3
+>>> 
+>>> a=34
+>>> b=54.46
+>>> print(float(a))
+34.0
+>>> print((b))
+54.46
+>>> print(int(b))
+54
+>>> 
